8313cb01beed9ae0e20cc080559fa146d465320b
John <ioannisg@newdell>
2015-02-22 19:44:40 +0100

    resolv.c update for mutiple interfaces, and for distributed dns64 add configuration and build structure in Makefile

---
+++ projects/net/ip64-conf.h
@@ -0,0 +1,23 @@
+/*
+ * ip64_conf.h
+ *
+ * Created: 2015-01-28 23:51:19
+ *  Author: ioannisg
+ */ 
+
+
+#ifndef IP64_CONF_H_
+#define IP64_CONF_H_
+
+#include "ip64/ip64-eth-interface.h"
+#include "ip64/ip64-null-driver.h"
+
+#define IP64_CONF_UIP_FALLBACK_INTERFACE ip64_eth_interface
+#define IP64_CONF_INPUT                  ip64_eth_interface_input
+
+#define IP64_CONF_ETH_DRIVER             ip64_null_driver
+
+#define IP64_CONF_DHCP                   1
+
+
+#endif /* IP64-CONF_H_ */
