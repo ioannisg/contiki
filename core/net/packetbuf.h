bf3af4941f1ccad9fb268dffb38c4082b3032a06
John <ioannisg@newdell>
2015-04-04 09:47:42 +0200

    Add a 6-byte null link address for ethernet/wifi Extend packet buffer attributes for wifi, ethernet and dual radio Add connect method on netstack_network api

--- core/net/packetbuf.h
+++ core/net/packetbuf.h
@@ -333,6 +333,11 @@ struct packetbuf_addr {
 #define PACKETBUF_ATTR_PACKET_TYPE_STREAM_END 3
 #define PACKETBUF_ATTR_PACKET_TYPE_TIMESTAMP 4
 
+#if WITH_WIFI_SUPPORT
+#define PACKETBUF_ATTR_PACKET_TYPE_ATIM 5
+#define PACKETBUF_ATTR_PACKET_TYPE_BCN	 6
+#endif
+
 enum {
   PACKETBUF_ATTR_NONE,
 
@@ -349,6 +354,8 @@ enum {
   PACKETBUF_ATTR_MAC_SEQNO,
   PACKETBUF_ATTR_MAC_ACK,
   PACKETBUF_ATTR_IS_CREATED_AND_SECURED,
+  
+  PACKETBUF_ATTR_RADIO_INTERFACE,
   
   /* Scope 1 attributes: used between two neighbors only. */
   PACKETBUF_ATTR_RELIABLE,
@@ -372,6 +379,15 @@ enum {
 #endif /* LLSEC802154_USES_EXPLICIT_KEYS */
 #endif /* LLSEC802154_SECURITY_LEVEL */
   
+ /* Scope 1 attributes for 802.3/802.11 link layers. */
+#if WITH_WIFI_SUPPORT
+  PACKETBUF_ATTR_MAC_TX_RSP_SEQNO,
+  PACKETBUF_ATTR_STA_PSM,
+#endif
+#if WITH_ETHERNET_SUPPORT
+  PACKETBUF_ATTR_ETH_PROTO,
+#endif
+	 
   /* Scope 2 attributes: used between end-to-end nodes. */
 #if NETSTACK_CONF_WITH_RIME
   PACKETBUF_ATTR_HOPS,
